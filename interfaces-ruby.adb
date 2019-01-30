30c9eb17e60b6852727aed39bae5162142bbba7a
Jesper Quorning <jesper.quorning@gmail.com>
2019-01-30 07:29:48 +0100

    Create interfaces-ruby.adb

---
+++ interfaces-ruby.adb
@@ -0,0 +1,13 @@
+--
+--  RUBY - Rapid Urban Busy Youngsters
+--  Interfaces.Ruby body file
+--
+
+package body Interfaces.Ruby is
+
+   procedure Dummy is
+   begin
+      null;
+   end Dummy;
+
+end Interfaces.Ruby;
